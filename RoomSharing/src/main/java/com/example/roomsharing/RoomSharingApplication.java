5e9d0ac54cc0e77afcc51fe4a30885c0cf044618
ravitejaponnekanti <139569497+ravitejaponnekanti@users.noreply.github.com>
2024-05-28 16:38:54 -0400

    Add files via upload

---
+++ RoomSharing/src/main/java/com/example/roomsharing/RoomSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomSharingApplication.class, args);
+	}
+
+}
